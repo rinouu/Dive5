bd1712a37cd5727a8e11526dcf58f3669644360e
Corine <corine@localhost.localdomain>
2021-03-05 14:27:40 +0100

    Exercice web application mis à jour 2

--- test.rb
+++ test.rb
@@ -1,30 +1,14 @@
 require 'webrick'
-require 'erb'
-
-
-erb_file = './web_appli.html.erb'
-html_file = File.basename(erb_file, '.erb')#=>"page.html"
-
-erb_str = File.read(erb_file)
-
-renderer = ERB.new(erb_str)
-result = renderer.result()
-
-File.open(html_file, 'w') do |f|
-  f.write(result)
-end
-
-path=File.expand_path'./web_appli.html'
 
 server = WEBrick::HTTPServer.new({
-  :DocumentRoot => path,
+  :DocumentRoot => '.',
   :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
   :Port => '3000',
 })
 ['INT', 'TERM'].each {|signal|
   Signal.trap(signal){ server.shutdown }
 }
-server.mount('/web_appli', WEBrick::HTTPServlet::ERBHandler, 'web_appli.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'web_appli.html.erb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 server.mount('/department.cgi', WEBrick::HTTPServlet::CGIHandler, 'department.rb')
 server.mount('/good_goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'good_goya.rb')
